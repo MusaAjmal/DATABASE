ed1bad021f590baf4c0b9cbf8b30353f95aa3074
Musa Ajmal <musaajmal56@gmail.com>
2024-05-23 15:37:42 +0500

    installed modules

---
+++ Test3 (1).sql
@@ -0,0 +1,21 @@
+use Project
+SELECT 
+  o.name AS Procedure_Name,
+  OBJECT_DEFINITION(o.[object_id]) AS Procedure_Definition
+FROM sys.objects o
+WHERE o.type IN ('P', 'PC') -- Filter for procedures and stored procedures
+ORDER BY Procedure_Name;
+go
+
+SELECT 
+    trig.name AS TriggerName,
+    OBJECT_NAME(trig.parent_id) AS TableName,
+    trig.create_date AS CreationDate,
+    trig.modify_date AS LastModifiedDate,
+    OBJECT_DEFINITION(trig.object_id) AS TriggerDefinition
+FROM 
+    sys.triggers AS trig
+INNER JOIN 
+    sys.objects AS obj ON trig.parent_id = obj.object_id
+WHERE 
+    trig.is_ms_shipped = 0;
